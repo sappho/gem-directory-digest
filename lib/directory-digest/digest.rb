72f4318b67794fa91cd399ddc4c7b07690571684
Andrew Heald <andrew@heald.uk>
2016-06-20 20:31:46 +0100

    Add file mirroring.

--- lib/directory-digest/digest.rb
+++ lib/directory-digest/digest.rb
@@ -1,13 +1,16 @@
 require 'openssl'
 require 'json'
+require 'fileutils'
 
 module DirectoryDigest
   # DirectoryDigest::Digest - Creates a SHA256 digest of a directory's content
   class Digest
+    attr_reader :directory
     attr_reader :directory_digest
     attr_reader :file_digests
 
-    def initialize(directory_digest, file_digests)
+    def initialize(directory, directory_digest, file_digests)
+      @directory = directory
       @directory_digest = directory_digest
       @file_digests = file_digests
     end
@@ -33,7 +36,7 @@ module DirectoryDigest
         end
         file_digests[path] = file_digest.hexdigest
       end
-      Digest.new(directory_digest.hexdigest, file_digests)
+      Digest.new(directory, directory_digest.hexdigest, file_digests)
     end
 
     def ==(other)
@@ -53,13 +56,43 @@ module DirectoryDigest
       }
     end
 
+    def mirror_from(other, log = proc { |_| })
+      log.call("synchronizing #{other.directory} to #{directory}")
+      files_copied = 0
+      files_deleted = 0
+      changes = changes_relative_to(other)
+      changes[:removed].merge(changes[:changed]).each do |path, _|
+        source_path = "#{other.directory}#{path}"
+        destination_path = "#{directory}#{path}"
+        destination_directory = File.dirname(destination_path)
+        unless Dir.exist?(destination_directory)
+          log.call("create directory #{destination_directory}")
+          FileUtils.makedirs(destination_directory)
+        end
+        log.call("copy #{source_path} to #{destination_path}")
+        File.open(source_path, 'rb') do |source_file|
+          File.open(destination_path, 'wb') do |destination_file|
+            destination_file.write(source_file.read(4096)) until source_file.eof?
+          end
+        end
+        files_copied += 1
+      end
+      changes[:added].each do |path, _|
+        destination_path = "#{directory}#{path}"
+        log.call("delete #{destination_path}")
+        File.delete(destination_path)
+        files_deleted += 1
+      end
+      log.call("synchronized - files copied: #{files_copied} - files deleted: #{files_deleted}")
+    end
+
     def to_json
-      JSON.pretty_generate(directory_digest: directory_digest, file_digests: file_digests)
+      JSON.pretty_generate(directory: directory, directory_digest: directory_digest, file_digests: file_digests)
     end
 
     def self.from_json(json)
       json = JSON.parse(json)
-      Digest.new(json['directory_digest'], json['file_digests'])
+      Digest.new(json['directory'], json['directory_digest'], json['file_digests'])
     end
   end
 end
--- spec/directory-digest/digest_spec.rb
+++ spec/directory-digest/digest_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
+require 'fileutils'
 require 'directory-digest/digest'
 
 module DirectoryDigest
   describe Digest do
     describe '.sha256' do
       it 'creates a SHA256 digest of all the files in a directory' do
-        digest = Digest.sha256('spec/data')
+        digest = Digest.sha256('spec/test-data/data')
         expect(digest.directory_digest).to eq '515613f6a36f6d313c99db44fe96c019ec729474fa53d44ab2a13b3eea09634b'
         expect(digest.file_digests.count).to eq 4
         expect(digest.file_digests['/test-4096.bin'])
@@ -19,7 +20,7 @@ module DirectoryDigest
       end
 
       it 'creates a SHA256 digest of some of the files in a directory (with an exclude)' do
-        digest = Digest.sha256('spec/data', '**/*', ['-\.txt$'])
+        digest = Digest.sha256('spec/test-data/data', '**/*', ['-\.txt$'])
         expect(digest.directory_digest).to eq '07076f7ddd861e2ef6b0510b6d8f08cfcc409380e90ecc2a3070b42db356305c'
         expect(digest.file_digests.count).to eq 2
         expect(digest.file_digests['/test-4096.bin'])
@@ -29,7 +30,7 @@ module DirectoryDigest
       end
 
       it 'creates a SHA256 digest of some of the files in a directory (with an exclude and cancelling include)' do
-        digest = Digest.sha256('spec/data', '**/*', ['-.*', '+\.bin$'])
+        digest = Digest.sha256('spec/test-data/data', '**/*', ['-.*', '+\.bin$'])
         expect(digest.directory_digest).to eq '07076f7ddd861e2ef6b0510b6d8f08cfcc409380e90ecc2a3070b42db356305c'
         expect(digest.file_digests.count).to eq 2
         expect(digest.file_digests['/test-4096.bin'])
@@ -41,24 +42,24 @@ module DirectoryDigest
 
     describe '#==' do
       it 'can spot equality of two directories' do
-        first_digest = Digest.sha256('spec/data')
-        second_digest = Digest.sha256('spec/data')
+        first_digest = Digest.sha256('spec/test-data/data')
+        second_digest = Digest.sha256('spec/test-data/data')
         expect(first_digest == second_digest).to eq true
       end
     end
 
     describe '#!=' do
       it 'can spot inequality of two directories' do
-        first_digest = Digest.sha256('spec/data')
-        second_digest = Digest.sha256('spec/alt-data')
+        first_digest = Digest.sha256('spec/test-data/data')
+        second_digest = Digest.sha256('spec/test-data/alt-data')
         expect(first_digest != second_digest).to eq true
       end
     end
 
     describe '#changes_relative_to' do
       it 'creates a valid report on the differences between directories' do
-        first_digest = Digest.sha256('spec/alt-data')
-        second_digest = Digest.sha256('spec/data')
+        first_digest = Digest.sha256('spec/test-data/alt-data')
+        second_digest = Digest.sha256('spec/test-data/data')
         differences = first_digest.changes_relative_to(second_digest)
         expect(differences.count).to eq 4
         expect(differences[:added].count).to eq 1
@@ -68,8 +69,8 @@ module DirectoryDigest
       end
 
       it 'creates a valid report on the lack of differences between directories' do
-        first_digest = Digest.sha256('spec/data')
-        second_digest = Digest.sha256('spec/data')
+        first_digest = Digest.sha256('spec/test-data/data')
+        second_digest = Digest.sha256('spec/test-data/data')
         differences = first_digest.changes_relative_to(second_digest)
         expect(differences.count).to eq 4
         expect(differences[:added].count).to eq 0
@@ -79,9 +80,28 @@ module DirectoryDigest
       end
     end
 
+    describe '#mirror_from' do
+      it 'mirrors files correctly' do
+        FileUtils.makedirs('test')
+        first_digest = Digest.sha256('spec/test-data/data')
+        second_digest = Digest.sha256('test')
+        second_digest.mirror_from(first_digest, proc { |message| puts message })
+        second_digest = Digest.sha256('test')
+        expect(first_digest == second_digest).to eq true
+        first_digest = Digest.sha256('spec/test-data/alt-data')
+        second_digest.mirror_from(first_digest, proc { |message| puts message })
+        second_digest = Digest.sha256('test')
+        expect(first_digest == second_digest).to eq true
+        first_digest = Digest.sha256('spec/test-data/empty')
+        second_digest.mirror_from(first_digest, proc { |message| puts message })
+        second_digest = Digest.sha256('test')
+        expect(first_digest == second_digest).to eq true
+      end
+    end
+
     describe '#to_json' do
       it 'returns valid JSON' do
-        digest = Digest.sha256('spec/data')
+        digest = Digest.sha256('spec/test-data/data')
         expect(digest.to_json).to match(/^{.*?e4314adaaaec5fd9392651f770d132f5eda6b104ed371d5ac5bbc8cb30e87fba.*}$/m)
       end
     end
@@ -89,12 +109,14 @@ module DirectoryDigest
     describe '.from_json' do
       it 'parses valid JSON' do
         json = {
+          directory: '/opt/test',
           directory_digest: '515613f6a36f6d313c99db44fe96c019ec729474fa53d44ab2a13b3eea09634b',
           file_digests: {
             '/test-path' => '792524edd412193c9d2e53734a5e95ee67fc3e502e64dc6f8e2ea53e87d30ad8'
           }
         }.to_json
         digest = Digest.from_json(json)
+        expect(digest.directory).to eq '/opt/test'
         expect(digest.directory_digest).to eq '515613f6a36f6d313c99db44fe96c019ec729474fa53d44ab2a13b3eea09634b'
         expect(digest.file_digests.count).to eq 1
         expect(digest.file_digests['/test-path'])
